81aadcb3ffa6e6c8ef2c070cd4812f35c13dec01
Abel-Moremi <abelmoremiz@gmail.com>
2019-10-10 10:47:50 +0200

    Update connector.java

--- src/studentsystem/connector.java
+++ src/studentsystem/connector.java
@@ -10,5 +10,20 @@ package studentsystem;
  * @author Zozo
  */
 public class connector {
-    
+    public static void main(String[] args) {
+        ConnectDb();
+    }
+
+    public static Connection ConnectDb() {
+        Connection connection = null;
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/meditech_appointment_system_database","root","");
+            //JOptionPane.showMessageDialog(null, "Connection Established");
+            return connection;
+        } catch (ClassNotFoundException | SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Connection not Established");
+        }
+        return connection;
+    }
 }
