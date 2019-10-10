0533ada0b5f04dedda6be88ba4f6609c1894042c
Abel-Moremi <abelmoremiz@gmail.com>
2019-10-10 11:05:29 +0200

    Update connector.java
    Filled in the import

--- src/studentsystem/connector.java
+++ src/studentsystem/connector.java
@@ -5,6 +5,11 @@
  */
 package studentsystem;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author Zozo
