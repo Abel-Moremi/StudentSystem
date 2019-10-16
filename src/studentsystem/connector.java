8ddaf0e7686fe2041926a93ce576118748b31964
Abel Moremi <abelmoremi@gmail.com>
2019-10-16 17:10:26 +0200

    A bit of house cleaning for a single page design

--- src/studentsystem/connector.java
+++ src/studentsystem/connector.java
@@ -22,9 +22,9 @@ public class connector {
     public static Connection ConnectDb() {
         Connection connection = null;
         try {
-            Class.forName("com.mysql.jdbc.Driver");
-            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/meditech_appointment_system_database","root","");
-            //JOptionPane.showMessageDialog(null, "Connection Established");
+            Class.forName("oracle.jdbc.driver.OracleDriver");
+            connection = DriverManager.getConnection("jdbc:oracle:thin:@10.0.18.2:1521:orcl","mor03625","mor03625");
+            JOptionPane.showMessageDialog(null, "Connection Established");
             return connection;
         } catch (ClassNotFoundException | SQLException ex) {
             JOptionPane.showMessageDialog(null, "Connection not Established");
