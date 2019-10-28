3bdd0102f18352075f2a9a8de860ab574b82a781
rogueninja <rk.morake18@gmail.com>
2019-10-28 14:37:06 +0200

    added Assessments class

---
+++ src/studentsystem/Assessments.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package studentsystem;
+
+/**
+ *
+ * @author rudi
+ */
+public class Assessments {
+   
+    int studentId;
+    String moduleId;
+    String assessment;
+    int mark;
+    
+    public Assessments(int studentId, String moduleId, String assessment, int mark){
+        this.studentId = studentId;
+        this.moduleId = moduleId;
+        this.assessment = assessment;
+        this.mark = mark;
+    }
+    
+    public int getStudentID(){
+        return this.studentId;
+    }
+    
+    public String getmoduleID(){
+        return this.moduleId;
+    }
+    
+    public String getAssessment(){
+        return this.assessment;
+    }
+    
+    public int getMark(){
+        return this.mark;
+    }
+}
